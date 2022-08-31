c9cc7b73f5dd5447be40bc2b7a810b171b3200de
Teewai57 <bobtoyoabasi@gmail.com>
2022-08-31 22:04:24 +0100

    fixing footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,15 +1,34 @@
 import React from 'react'
 import "./Footer.css"
-import {MdOutlineEmail} from "react-icons/md"
-import {FaLinkedin} from "react-icons/fa"
-import {BsWhatsapp} from "react-icons/bs"
+import { MdOutlineEmail } from "react-icons/md"
+import { FaLinkedin } from "react-icons/fa"
+import { BsWhatsapp } from "react-icons/bs"
 
 
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer_logo'>Teewai</a>
 
+      <div>
+        <a href="#" className='footer_logo'>Teewai</a>
+      </div>
+      <div className="footer_socials">
+        <a href="https://ng.linkedin.com/in/toyoabasi-bob-9835a7245?trk=people-guest_people_search-card" target='_blank'><FaLinkedin /></a>
+        <a href="https://wa.me/08162340257" target="_blank"><BsWhatsapp /></a>
+        <a href="mailto:bobtoyoabasi@gmail.com" target="_blank"><MdOutlineEmail /></a>
+      </div>
+      <div className="footer_copyrights">
+        <small>&copy; Toyo. All rights reserved.</small>
+      </div>
+
+
+    </footer>
+  )
+}
+
+export default Footer
+
+{/* 
       <ul className='permalinks'>
         <li><a href="#">Home</a></li>
         <li><a href="#about">About</a></li>
@@ -18,18 +37,18 @@ const Footer = () => {
         <li><a href="#portfolio">Portfolio</a></li>
         <li><a href="#testimonials">Testimonials</a></li>
         <li><a href="#contact">Contact Me</a></li>
-      </ul>
-      <div className="footer_socials">
-        <a href="https://ng.linkedin.com/in/toyoabasi-bob-9835a7245?trk=people-guest_people_search-card" target='_blank'><FaLinkedin /></a>
-        <a href="https://wa.me/08162340257" target="_blank"><BsWhatsapp /></a>
-        <a href="mailto:bobtoyoabasi@gmail.com"  target="_blank"><MdOutlineEmail /></a>
-      </div>
+      </ul> */}
+  // <footer>
+  //   <div>
+  //   <a href="#" className='footer_logo'>Teewai</a>
+  //   </div>
+  //   <div className="footer_socials">
+  //     <a href="https://ng.linkedin.com/in/toyoabasi-bob-9835a7245?trk=people-guest_people_search-card" target='_blank'><FaLinkedin /></a>
+  //     <a href="https://wa.me/08162340257" target="_blank"><BsWhatsapp /></a>
+  //     <a href="mailto:bobtoyoabasi@gmail.com"  target="_blank"><MdOutlineEmail /></a>
+  //   </div>
 
-      <div className="footer_copyrights">
-        <small>&copy; Toyo. All rights reserved.</small>
-      </div>
-    </footer>
-  )
-}
-
-export default Footer
+  //   <div className="footer_copyrights">
+  //     <small>&copy; Toyo. All rights reserved.</small>
+  //   </div>
+  // </footer>
